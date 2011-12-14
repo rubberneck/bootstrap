1be336a601bdb069b8dd71cef460e6b9c1902224
Arnold Daniels <arnold@adaniels.nl>
2011-12-14 03:29:25 +0100

    Build uglified js

---
+++ js/min/loader-maskedinput.js
@@ -0,0 +1,2 @@
+/* APPLY STANDARD TO INPUT ELEMENTS WITH DATA-MASK
+ * =============================================== */$(function(){$("input[data-mask]").each(function(){$(this).mask($(this).attr("data-mask"))})});
---
+++ js/min/loader-placeholder.js
@@ -0,0 +1,2 @@
+/* APPLY STANDARD TO INPUT ELEMENTS WITH PLACEHOLDER
+ * ================================================= */$(function(){NATIVE_SUPPORT||$("input[placeholder], textarea[placeholder]").placeholder()});
